02075ff7e66737be31c5dc339c4c840bd29dedc5
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-16 08:25:52 +0530

    feat(get request): all get requst GET, POST, PUT, DELETE

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// get request 
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Got a GET request');
+})
+
+app.post('/items', (req, res) => {
+    res.send('Got a POST request');
+})
+
+app.put('/items/:id', (req, res) => {
+    res.send('Got a PUT request');
+})
+
+app.delete('/items/:id', (req, res) => {
+    res.send('Got a DELETE request');
 })
 
 app.listen(port, () => {
